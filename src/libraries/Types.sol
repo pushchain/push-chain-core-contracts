e644e3fef8c1aaf813f9132f97233ad79bf72039
Zaryab <zaryabafser2000@gmail.com>
2025-06-25 12:20:10 +0400

    beautify

--- src/libraries/Types.sol
+++ src/libraries/Types.sol
@@ -3,9 +3,9 @@ pragma solidity 0.8.26;
 
 // User Struct
 struct UniversalAccountId {
-    string chainNamespace; // Chain namespace identifier of the owner account (e.g., "eip155" or "solana")
-    string chainId; // Chain ID of the source chain of the owner of this UEA.
-    bytes owner; // Owner's public key or address in bytes format
+    string chainNamespace;  // Chain namespace identifier of the owner account (e.g., "eip155" or "solana")
+    string chainId;         // Chain ID of the source chain of the owner of this UEA.
+    bytes owner;            // Owner's public key or address in bytes format
 }
 
 // Signature verification types
@@ -16,15 +16,15 @@ enum VerificationType {
 
 struct UniversalPayload {
     // Core execution parameters
-    address to; // Target contract address to call
-    uint256 value; // Native token amount to send
-    bytes data; // Call data for the function execution
-    uint256 gasLimit; // Maximum gas to be used for this tx (caps refund amount)
-    uint256 maxFeePerGas; // Maximum fee per gas unit
-    uint256 maxPriorityFeePerGas; // Maximum priority fee per gas unit
-    uint256 nonce; // Chain ID where this should be executed
-    uint256 deadline; // Timestamp after which this payload is invalid
-    VerificationType vType; // Type of verification to use before execution (signedVerification or universalTxVerification)
+    address to;                    // Target contract address to call
+    uint256 value;                 // Native token amount to send
+    bytes data;                    // Call data for the function execution
+    uint256 gasLimit;              // Maximum gas to be used for this tx (caps refund amount)
+    uint256 maxFeePerGas;          // Maximum fee per gas unit
+    uint256 maxPriorityFeePerGas;  // Maximum priority fee per gas unit
+    uint256 nonce;                 // Chain ID where this should be executed
+    uint256 deadline;              // Timestamp after which this payload is invalid
+    VerificationType vType;        // Type of verification to use before execution (signedVerification or universalTxVerification)
 }
 
 // Hash of keccak256("UniversalPayload(address to,uint256 value,bytes data,uint256 gasLimit,uint256 maxFeePerGas,uint256 maxPriorityFeePerGas,uint256 nonce,uint256 deadline,uint8 vType)")
