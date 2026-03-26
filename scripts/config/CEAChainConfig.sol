bf5de2ba88002fd9ce7ac56c1e21974a80907825
Zaryab <zaryabafser2000@gmail.com>
2026-03-26 17:27:36 +0530

    vault_setup

--- scripts/config/CEAChainConfig.sol
+++ scripts/config/CEAChainConfig.sol
@@ -49,7 +49,7 @@ abstract contract CEAChainConfig {
     function _arbitrumSepolia() private pure returns (Config memory) {
         return Config({
             owner: 0xD854DDe7C58eC1B405E6577F48a7cC5b5E6EF317,
-            vault: 0xE52AC4f8DD3e0263bDF748F3390cdFA1f02be881,
+            vault: 0x233B1B1B378eb0Aa723097634025A47C4b73A8F7,
             universalGateway: 0x2cd870e0166Ba458dEC615168Fd659AacD795f34,
             ceaFactoryProxy: 0x88DC189275078Cf509E4Cc773F089c8ad07b7EA2
         });
@@ -62,7 +62,7 @@ abstract contract CEAChainConfig {
     function _ethSepolia() private pure returns (Config memory) {
         return Config({
             owner: 0xe520d4A985A2356Fa615935a822Ce4eFAcA24aB6,
-            vault: 0xE52AC4f8DD3e0263bDF748F3390cdFA1f02be881,
+            vault: 0xD019Eb12D0d6eF8D299661f22B4B7d262eD4b965,
             universalGateway: 0x05bD7a3D18324c1F7e216f7fBF2b15985aE5281A,
             ceaFactoryProxy: 0x8ED594A83301FEc545fC6c19fc12cF7111777029
         });
@@ -75,7 +75,7 @@ abstract contract CEAChainConfig {
     function _baseSepolia() private pure returns (Config memory) {
         return Config({
             owner: 0x52DEA34AfAaD33Bb16675ED527b1ed80E83ffb09,
-            vault: 0xE52AC4f8DD3e0263bDF748F3390cdFA1f02be881,
+            vault: 0xb4Ba4D5542D1dD48BD3589543660B265B41f16CB,
             universalGateway: 0xFD4fef1F43aFEc8b5bcdEEc47f35a1431479aC16,
             ceaFactoryProxy: 0x0A75ca7736b488Eb41675ADc3b3156BACF659F55
         });
